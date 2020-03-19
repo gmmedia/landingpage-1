ee04d061b6cdd57a858e8aaf73ecaabf94efc6d7
Jochen Gererstorfer <7474702+gmmedia@users.noreply.github.com>
2020-03-19 16:01:27 +0100

    Link geändert

--- components/headerComp.vue
+++ components/headerComp.vue
@@ -6,6 +6,9 @@
       <v-btn text>Link One</v-btn>
       <v-btn text>Link Two</v-btn>
       <v-btn text>Link Three</v-btn>
+      <v-btn color="primary" dark href="https://j0e.org/tipp/Divi" rel="nofollow noopener" target="_blank">
+        Hole dir DIVI
+      </v-btn>
     </v-toolbar-items>
   </v-app-bar> 
 </template>
